c14d433b0bda277d00b15a210ac6df6718cd7ff7
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-05-27 04:08:11 +0000

    [Finish]いいね機能の追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,27 +1,35 @@
 class FavoritesController < ApplicationController
-  
+
   def create
     # いいねする本をURLから特定する
     book = Book.find(params[:book_id])
-    # 新しいいいねを作成する、扱うことを許可されたカラムの情報を取得する
-    favorite = Favorite.new(favorite_params)
-    # いいねする人のidをcurrent_userメソッドで定義
+    # 新しいいいねを作成する、直接newする内容（カラム名、入れる情報名）を指定する
+    favorite = Favorite.new(book_id: book.id)
+    # いいねする人のidをcurrent_userメソッドで定義、これでfavoriteモデルの必要なカラムすべての情報を取得・定義
     favorite.user_id = current_user.id
     # いいね情報を保存
     favorite.save
     # ひとつ前のページに戻る
-    redirect_back
+    redirect_back fallback_location: books_path
   end
-  
+
   def destroy
+    # いいねの削除をする本をURLから特定する
+    book = Book.find(params[:book_id])
+    # 削除するいいねを作成する、直接newする内容（カラム名、入れる情報名）を指定する
+    favorite = Favorite.find_by(book_id: book.id)
+    favorite.user_id = current_user.id
+    favorite.destroy
+    redirect_back fallback_location: books_path
+
   end
-  
-  
-  private
-  
+
+  # いいねに保存や取り出すときにデータを制限する必要はないため、SPは不要
+  # private
+
   # createするときにモデルのテーブルを扱うために定義が必要
-  def favorite_params
-    params.require(:favorite).permit(:user_id, :book_id)
-  end    
+  # def favorite_params
+    # params.require(:favorite).permit(:user_id, :book_id)
+  # end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,11 @@ class Book < ApplicationRecord
   # カンマの後のスペースはどっちでもいい？
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
+  
+  # いいねの表示をすでにいいねをしたか否かで変更するためのメソッド定義
+  # これを使うのはbookのビュー内なので、Bookモデルに定義
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
