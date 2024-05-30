2ba97a3170b47188a07f9d5d909460f1c2aa1a09
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-05-30 04:40:13 +0000

    [Finish]all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_29_020453) do
+ActiveRecord::Schema.define(version: 2024_05_28_075543) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
