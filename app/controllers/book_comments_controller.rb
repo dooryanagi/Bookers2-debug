9b90fe9bb7719c9b6c268c3874829bf3f216a59b
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-06-03 02:17:45 +0000

    [Finish]コメントの非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,21 +4,28 @@ class BookCommentsController < ApplicationController
     # コメントするための本をURLをもとに探してくる
     @book = Book.find(params[:book_id])
     # 許可されたパラメータでコメントを新たに作る
-    comment = BookComment.new(comment_params)
+    # コメントの非同期通信化のためにcomment→@comment
+    @comment = BookComment.new(comment_params)
     # 必要なuser_id情報をcurrent_userメソッドで取得する
     # @bookとの関連付けが必要
-    comment.book_id = @book.id
+    @comment.book_id = @book.id
     # 誰がコメントしたかの情報も必要
-    comment.user_id = current_user.id
+    @comment.user_id = current_user.id
     # コメントを保存する
-    comment.save
+    @comment.save
     # 前のページに戻れなかった場合はそのまま詳細ページにいるようにする
-    redirect_back fallback_location: book_path(params[:book_id])
+    # コメントの非同期通信化
+    # redirect_back fallback_location: book_path(params[:book_id])
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    # linktoで削除するコメントのidを取得しているので、ここではparams[:id]で良い
+    @comment = BookComment.find(params[:id])
+    # jsファイル内で読み取るので@bookを定義しておく
+    @book = @comment.book
+    @comment.destroy
+    # コメントの非同期通信化
+    # redirect_to book_path(params[:book_id])
   end
 
   private
